a56c8a183301423201fdf42927300c23c07c72f0
unknown <tirritoroberto@gmail.com>
2021-11-18 17:29:53 +0100

    pulsanti da tastiera

--- 18.11/esercizio/script.js
+++ 18.11/esercizio/script.js
@@ -10,26 +10,65 @@ const prot = document.querySelector(".prot");
 let backgroundBoard = 0;
 let backgroundFloor = 0;
 
-dx.addEventListener('click', ()=>{
+dx.addEventListener('click', () => {
     prot.classList.remove('sx');
-    base.style.backgroundPositionX=`${backgroundBoard-=3}px`;
-    floor.style.backgroundPositionX=`${backgroundFloor-=20}px`;
+    base.style.backgroundPositionX = `${backgroundBoard -= 3}px`;
+    floor.style.backgroundPositionX = `${backgroundFloor -= 20}px`;
 
     prot.classList.toggle('move');
-    setTimeout(()=>{prot.classList.toggle('move')}, 50)
+    setTimeout(() => { prot.classList.toggle('move') }, 250)
 })
 
-sx.addEventListener('click', ()=>{
+sx.addEventListener('click', () => {
     prot.classList.add('sx');
-    base.style.backgroundPositionX=`${backgroundBoard+=3}px`;
-    floor.style.backgroundPositionX=`${backgroundFloor+=20}px`;
+    base.style.backgroundPositionX = `${backgroundBoard += 3}px`;
+    floor.style.backgroundPositionX = `${backgroundFloor += 20}px`;
 
     prot.classList.toggle('move');
-    setTimeout(()=>{prot.classList.toggle('move')}, 50)
+    setTimeout(() => { prot.classList.toggle('move') }, 250)
 })
 
 
-up.addEventListener('click',()=>{
+up.addEventListener('click', () => {
     prot.classList.toggle('animate__bounce');
-    setTimeout(()=>{prot.classList.toggle('animate__bounce')}, 2000)
+    setTimeout(() => { prot.classList.toggle('animate__bounce') }, 2000)
 })
+
+document.onkeydown = arrowPress;
+
+function arrowPress(event) {
+    switch (event.keyCode) {
+        case 39:
+            dxMove();
+            break;
+        case 37:
+            sxMove();
+            break;
+        case 38:
+            upMove()
+            break;
+    }}
+
+
+    const dxMove = () => {
+        prot.classList.remove('sx');
+        base.style.backgroundPositionX = `${backgroundBoard -= 3}px`;
+        floor.style.backgroundPositionX = `${backgroundFloor -= 20}px`;
+
+        prot.classList.toggle('move');
+        setTimeout(() => { prot.classList.toggle('move') }, 250)
+    }
+
+    const sxMove = () => {
+        prot.classList.add('sx');
+        base.style.backgroundPositionX = `${backgroundBoard += 3}px`;
+        floor.style.backgroundPositionX = `${backgroundFloor += 20}px`;
+
+        prot.classList.toggle('move');
+        setTimeout(() => { prot.classList.toggle('move') }, 250)
+    }
+
+    const upMove = () =>{
+        prot.classList.toggle('animate__bounce');
+        setTimeout(() => { prot.classList.toggle('animate__bounce') }, 1000)
+    }
